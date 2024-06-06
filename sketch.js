3787ba009022c48d439ece07d34f7c59f786477c
tarcesio137 <00001096934711sp@al.educacao.sp.gov.br>
2024-06-06 15:28:57 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let cor;
+let posicaoHorizontal; //x
+let posicaoVertical; //y
+
+
+function setup() {
+    createCanvas(400, 400);
+    background(color(100, 0, 0));
+    cor = color(random(0, 255), random(0, 255), random(0,255));
+posicaoHorizontal = 200;
+  posicaoVertical = 200;
+    }
+    
+function draw() {
+fill(cor)
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  
+  if(mouseX < posicaoHorizontal) posicaoHorizontal = posicaoHorizontal -=random(0, 3);
+  if(mouseX > posicaoHorizontal) posicaoHorizontal = posicaoHorizontal += random(0, 3);
+  
+  if(mouseY < posicaoVertical) posicaoVertical = posicaoVertical -= (random(0, 3))
+  if(mouseY > posicaoVertical) posicaoVertical = posicaoVertical += random(0, 3);
+  
+  if(mouseIsPressed)
+     cor = color(random(0, 255), random(0, 255), random(0,255), random(0, 100));
+}
